ba6116f0baa3ece7667110e5963e81a533a8be5d
Robert <robertyeam@gmail.com>
2021-03-27 15:09:58 -0700

    implemented password generating function

--- script.js
+++ script.js
@@ -12,3 +12,62 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+function getRandNum(length){
+  return Math.floor(Math.random() *length);
+}
+
+
+function generatePassword(){
+  console.log("generate password");
+  var len=prompt("Length of password(at least 8, no more than 128):");
+  
+  len = parseInt(len);
+  if(isNaN(len)){
+    alert("Not a number");
+    generatePassword();
+  }
+
+  var includeUpperCase = confirm("Include upper case letters");
+  var includeNumbers = confirm("Include numbers");
+  var includeSpecialChars = confirm("Include special characters");
+
+  console.log("Length: "+len+"\nUppercase: "+includeUpperCase+"\nNumbers: "+includeNumbers+ "\nSpecial Characters: "+includeSpecialChars);
+
+
+  /*
+  var password = "";
+  for(var i=0;i<len; i++){
+
+  }
+  */
+
+  //console.log("\"", "\\");
+  var letters = "abcdefghijklmnopqrstuvwxyz";
+  var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var digits="0123456789";
+  var specialChars=" !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+
+  var options = letters;
+  if(includeUpperCase){
+    options += upper;
+  }
+  if(includeNumbers){
+    options += digits;
+  }
+  if(includeSpecialChars){
+    options += specialChars;
+  }
+  console.log("Available: ",options);
+
+  //
+  var password = "";
+  for(var i=0;i<len; i++){
+    password += options[getRandNum(options.length)];
+  }
+
+  console.log("Password", password);
+  return password;
+}
